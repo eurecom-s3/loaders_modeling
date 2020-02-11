0eed889512183253af81a671aa4f99b93d222bd7
Dario Nisi <dario.nisi@eurecom.fr>
2020-02-11 11:41:19 +0100

    Fixed tests to use new interface

--- tests/operators/bitwise.py
+++ tests/operators/bitwise.py
@@ -1,6 +1,6 @@
 import z3
 
-from parsers import parser, statements
+from parsers import Parser
 from backends import *
 
 testfile = "tests/operators/bitwise.mod"
@@ -16,18 +16,18 @@ def parse_file(f):
         if result:
             print(result)
 
-with open(testfile, "r") as fp:
-    parse_file(fp)
-    exec_statements(statements)
-    solver = generate_solver()
-    input = variables['VARIABLE']
-    v1 = variables['VARA']
-    v2 = variables['VARB']
-    v3 = variables['VARC']
+parser = Parser()
+parser.parse_file(testfile)
+exec_statements(parser.statements)
+solver = generate_solver()
+input = variables['VARIABLE']
+v1 = variables['VARA']
+v2 = variables['VARB']
+v3 = variables['VARC']
 
-    solver.add(input == 0xdeadbeef)
-    assert(solver.check() == z3.sat)
-    model = solver.model()
-    assert(model.eval(v1).as_long() == 0x11001)
-    assert(model.eval(v2).as_long() == 0xdfadbeef)
-    assert(model.eval(v3).as_long() == 0x21524110)
+solver.add(input == 0xdeadbeef)
+assert(solver.check() == z3.sat)
+model = solver.model()
+assert(model.eval(v1).as_long() == 0x11001)
+assert(model.eval(v2).as_long() == 0xdfadbeef)
+assert(model.eval(v3).as_long() == 0x21524110)
